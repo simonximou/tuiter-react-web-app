f417fd6276463f1832f7039f445b2e2660a3b6fa
Xi Zhang <71995672+simonximou@users.noreply.github.com>
2022-11-30 00:59:54 -0800

    a9 fall22 5610

--- src/tuiter/post-summary-list/post-summary-item.js
+++ src/tuiter/post-summary-list/post-summary-item.js
@@ -16,7 +16,7 @@ const PostSummaryItem = (
                 <div className="col-10">
                     <div>{post.userName} . {post.time}</div>
                     <div className="fw-bolder">{post.topic}</div>
-                    <div>{post.title}</div>
+                    <div>{post.tuit}</div>
                 </div>
                 <div className="col-2">
                     <img width={70} className="float-end rounded-3" src={`${post.image}`} alt={""}/>
